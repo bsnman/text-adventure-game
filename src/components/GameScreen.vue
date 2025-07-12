1c5e01a3b66c5649f458dfc541622a0afc07ac0c
Radih Jr Paraguas <radihjrprgs@gmail.com>
2025-07-13 08:50:06 +1000

    Setup structure

---
+++ src/components/GameScreen.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>Text Adventure Game</h1>
+    <p>Welcome to the text adventure game! Your journey begins here.</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
