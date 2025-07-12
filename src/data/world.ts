c38567a09463a9f63dc35303ab791a4848e2a963
Radih Jr Paraguas <radihjrprgs@gmail.com>
2025-07-13 09:09:51 +1000

    Add basic world rooms

---
+++ src/data/world.ts
@@ -0,0 +1,18 @@
+export const world = {
+  start: {
+    description: "You are in a dimly lit room. There’s a door to the north.",
+    exits: { north: "hallway" },
+  },
+  hallway: {
+    description: "You are in a long hallway. Doors lead east and west.",
+    exits: { south: "start", east: "library", west: "kitchen" },
+  },
+  library: {
+    description: "You enter a dusty library filled with old books.",
+    exits: { west: "hallway" },
+  },
+  kitchen: {
+    description: "The kitchen smells of old food. There’s a knife here.",
+    exits: { east: "hallway" },
+  },
+};
