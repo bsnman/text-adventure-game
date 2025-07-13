0997771ddd375559d65394bc0d70b20909dabbaa
bsnman <radihjrprgs@gmail.com>
2025-07-13 13:10:29 +1000

    Integrate Vuetify (#5)

--- src/components/GameScreen.vue
+++ src/components/GameScreen.vue
@@ -5,19 +5,20 @@ const game = useGameStore()
 </script>
 
 <template>
-  <div>
-    <h1>Text Adventure Game</h1>
-    <p>{{ game.room.description }}</p>
-    <ul>
-      <li v-for="direction in Object.keys(game.room.exits)" :key="direction">
-        {{ direction }}
-      </li>
-    </ul>
-  </div>
+  <v-card class="mx-auto" max-width="400">
+    <v-card-title>Text Adventure Game</v-card-title>
+    <v-card-text>
+      <p>{{ game.room.description }}</p>
+      <v-list>
+        <v-list-item
+          v-for="direction in Object.keys(game.room.exits)"
+          :key="direction"
+        >
+          <v-list-item-title>{{ direction }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import './style.css'
 
 const app = createApp(App)
 app.use(createPinia())
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
 app.mount('#app')
