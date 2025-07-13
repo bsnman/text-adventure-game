e55bb8ac3f15a6afcdd0fbd38c2f46353965066c
bsnman <radihjrprgs@gmail.com>
2025-07-13 14:33:40 +1000

    feat: add command input and move buttons (#6)

--- src/components/GameScreen.vue
+++ src/components/GameScreen.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useGameStore } from '../store/game'
 
 const game = useGameStore()
+const command = ref('')
+
+function handleCommand() {
+  const input = command.value.trim().toLowerCase()
+  const match = input.match(/^(?:go\s+)?(\w+)/)
+  if (match) {
+    game.move(match[1])
+  }
+  command.value = ''
+}
 </script>
 
 <template>
@@ -9,14 +20,23 @@ const game = useGameStore()
     <v-card-title>Text Adventure Game</v-card-title>
     <v-card-text>
       <p>{{ game.room.description }}</p>
-      <v-list>
-        <v-list-item
+      <div class="my-2">
+        <v-btn
           v-for="direction in Object.keys(game.room.exits)"
           :key="direction"
+          class="ma-1"
+          @click="game.move(direction)"
         >
-          <v-list-item-title>{{ direction }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
+          Go {{ direction }}
+        </v-btn>
+      </div>
+      <v-text-field
+        v-model="command"
+        label="Command"
+        placeholder="e.g., go north"
+        hide-details
+        @keyup.enter="handleCommand"
+      ></v-text-field>
     </v-card-text>
   </v-card>
 </template>
